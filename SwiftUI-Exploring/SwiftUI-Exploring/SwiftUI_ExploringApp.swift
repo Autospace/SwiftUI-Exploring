688256f92800d6e60b10423215fa8c10caf8a3bc
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2021-03-03 18:41:54 +0300

    Create project

---
+++ SwiftUI-Exploring/SwiftUI-Exploring/SwiftUI_ExploringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExploringApp.swift
+//  SwiftUI-Exploring
+//
+//  Created by Alex Mostovnikov on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExploringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
